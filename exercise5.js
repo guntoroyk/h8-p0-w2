abd4fe1dc6478c59b0e35e905927621d0d6bbe0f
igun369 <guntoro.gyk@gmail.com>
2019-07-05 14:00:52 +0700

    edit exercise4 to validate the date and year

--- exercise5.js
+++ exercise5.js
@@ -40,7 +40,7 @@ console.log()
 var word3 = 'wow JavaScript is so cool';
 var exampleFirstWord3 = word3.substring(0, 3);
 var secondWord3 = word3.substring(4, 14); 
-var thirdWord3 = word3.substring(15, 17); 
+var thirdWord3 = word3.substring(15, 17);
 var fourthWord3 = word3.substring(18, 20);
 var fifthWord3 = word3.substring(21, 25);
 
